1f8e5b802db8bd07c6979088b726cf2700d46ed3
Admin <dima16kadam@mail.ru>
2025-04-08 16:45:01 +0300

    first commit

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ use Kubinskiy\project\PizzaStore;
 use Kubinskiy\project\PizzaStoreOrders;
 
 $premiumFabrik = new PizzaStoreOrders();
-$premiumFabrik->orderPizza("Пицца с сыром");
+$premiumFabrik->orderPizza("Пицца цезарь");
